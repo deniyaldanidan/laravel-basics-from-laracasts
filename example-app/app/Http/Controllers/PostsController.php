d9a155d3048a96b0286d493991d4adba72288681
deniyaldanidan <deniyaldanidan@gmail.com>
2021-03-12 22:29:43 +0530

    Lesson - Hello Eloquent

--- example-app/app/Http/Controllers/PostsController.php
+++ example-app/app/Http/Controllers/PostsController.php
@@ -1,15 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function retrieve($postId)
     {
-      $post = \DB::table('posts')->where('slug', $postId)->first();
+    //  $post = \DB::table('posts')->where('slug', $postId)->first();
+    $post = Post::where('slug', $postId)->firstOrFail();
       //dd($post->body);
+      // if the blog is not there abort
+      /*
+      if (! $post) {
+        abort(404);
+      }
+      */
       return view('posts', [
         "blogHead" => $postId,
         "body" => $post->body
